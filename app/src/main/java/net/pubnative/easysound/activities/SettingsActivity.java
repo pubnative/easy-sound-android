571e07cd3262dd615c0de1abb722abe608e6fd8b
eros902002@googlemail.com <eros902002@googlemail.com>
2018-04-16 14:59:55 +0200

    Fix settings return button

--- app/src/main/java/net/pubnative/easysound/activities/SettingsActivity.java
+++ app/src/main/java/net/pubnative/easysound/activities/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import net.pubnative.easysound.R;
 import net.pubnative.easysound.fragments.SettingsFragment;
@@ -28,5 +29,17 @@ public class SettingsActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.container, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
